5c2e17b548388f45556ab73e81fa79f00680f211
Jeongsang <jeongsang.yoo@icon.foundation>
2018-08-09 17:33:28 +0900

    change layout file name

--- app/src/main/java/foundation/icon/iconex/wallet/menu/appInfo/OSSFragment.java
+++ app/src/main/java/foundation/icon/iconex/wallet/menu/appInfo/OSSFragment.java
@@ -29,7 +29,7 @@ public class OSSFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_os, container, false);
+        View v = inflater.inflate(R.layout.fragment_oss, container, false);
 
         WebView webView = v.findViewById(R.id.webview);
         webView.loadUrl("file:///android_asset/open_source_licenses.html");
