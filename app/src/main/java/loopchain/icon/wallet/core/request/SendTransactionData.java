0748b886b01e55c374e7ca40963e542420dd90b1
Jeongsang <jeongsang.yoo@icon.foundation>
2018-08-29 18:02:19 +0900

    Apply ICON API v3

--- app/src/main/java/loopchain/icon/wallet/core/request/SendTransactionData.java
+++ app/src/main/java/loopchain/icon/wallet/core/request/SendTransactionData.java
@@ -1,35 +1,44 @@
 package loopchain.icon.wallet.core.request;
 
+import android.util.Log;
+
 import com.google.gson.JsonObject;
 
+import org.spongycastle.util.encoders.Hex;
+
+import java.math.BigInteger;
+
 import loopchain.icon.wallet.core.Constants;
+import loopchain.icon.wallet.service.crypto.PKIUtils;
 import loopchain.icon.wallet.service.crypto.SendTransactionSigner;
 
 public class SendTransactionData extends RequestData {
 
-	private static final String FEE = "0x2386f26fc10000";
-	private static final String NONCE = "8367273";
+    private static final String FEE = "0x2386f26fc10000";
+    private static final String VERSION = "0x3";
+    private static final String NONCE = "0x1";
 
-    public SendTransactionData(String id, String timestamp, String from, String to, String value, String fee, String hexPrivateKey) {
+    public SendTransactionData(int id, String timestamp, String from, String to, String value, String stepLimit, String hexPrivateKey) {
         this.method = Constants.METHOD_SENDTRANSACTION;
         this.id = id;
-        
+
 //        String timestamp = getTimeStamp();
-        
+
         SendTransactionSigner signer;
-        signer = new SendTransactionSigner(FEE, from, timestamp, to, value, NONCE);
+        signer = new SendTransactionSigner(VERSION, from, to, value, stepLimit, timestamp, "0x3", NONCE);
 
         String txHash = signer.getTxHash();
         String signature = signer.getSignature(txHash, hexPrivateKey);
-        
+
         JsonObject params = new JsonObject();
+        params.addProperty("version", "0x3");
         params.addProperty("from", from);
         params.addProperty("to", to);
         params.addProperty("value", value);
-        params.addProperty("fee", fee);
+        params.addProperty("stepLimit", stepLimit);
         params.addProperty("timestamp", timestamp);
+        params.addProperty("nid", "0x3");
         params.addProperty("nonce", NONCE);
-        params.addProperty("tx_hash", txHash);
         params.addProperty("signature", signature);
 
         this.params = params;
--- app/src/main/java/loopchain/icon/wallet/service/crypto/SendTransactionSigner.java
+++ app/src/main/java/loopchain/icon/wallet/service/crypto/SendTransactionSigner.java
@@ -1,48 +1,47 @@
 package loopchain.icon.wallet.service.crypto;
 
+import android.util.Log;
+
 import java.math.BigInteger;
 
 import loopchain.icon.wallet.core.Constants;
 
 public class SendTransactionSigner {
+    private static final String TAG = SendTransactionSigner.class.getSimpleName();
 
-	private String _method = Constants.METHOD_SENDTRANSACTION;
-	private byte[] _tbs;
-	
-	public SendTransactionSigner(String fee, String from, String timestamp, String to, String value, String nonce) {
-		_tbs = makeTbs(fee, from, timestamp, to, value, nonce);
-	}
-	
-	public SendTransactionSigner(String fee, String from, String timestamp, String to, String value) {
-		_tbs = makeTbs(fee, from, timestamp, to, value, null);
-	}
-	
-	private byte[] makeTbs(String fee, String from, String timestamp, String to, String value, String nonce)  {
-		String tbs = _method + ".fee." + fee + ".from." + from;
-		if(nonce != null)
-			tbs = tbs + ".nonce." + nonce;
-		tbs = tbs + ".timestamp." + timestamp + ".to." + to + ".value." + value;
-		return tbs.getBytes();
-	}
-		
-	public String getTxHash() {	
-		try {
-			byte[] hash = PKIUtils.hash(_tbs, PKIUtils.ALGORITHM_HASH);
-			return PKIUtils.hexEncode(hash);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+    private String _method = Constants.METHOD_SENDTRANSACTION;
+    private byte[] _tbs;
 
-	public String getSignature(String txHash, String hexPrivateKey) {
-		byte[] privateKey = PKIUtils.hexDecode(hexPrivateKey);
-		byte[] hash = PKIUtils.hexDecode(txHash);
-		BigInteger[] sign = PKIUtils.sign(hash, privateKey);
-		byte[] publicKey = PKIUtils.getPublicKeyFromPrivateKey(privateKey, true);
+    public SendTransactionSigner(String version, String from, String to, String value, String stepLimit, String timestamp, String nid, String nonce) {
+        _tbs = makeTbs(version, from, to, value, stepLimit, timestamp, nid, nonce);
+    }
 
-		byte recoveryId = PKIUtils.getRecoveryId(sign, hash, publicKey);
-		byte[] signData = PKIUtils.getSignature(sign[0], sign[1], new byte[]{recoveryId});
-		return PKIUtils.b64Encode(signData);
-	}
+    private byte[] makeTbs(String version, String from, String to, String value, String stepLimit, String timestamp, String nid, String nonce) {
+        String tbs = _method + ".from." + from + ".nid." + nid + ".nonce." + nonce + ".stepLimit." + stepLimit
+                + ".timestamp." + timestamp + ".to." + to  + ".value." + value + ".version." + version;
+        Log.d(TAG, "tbs=" + tbs);
+
+        return tbs.getBytes();
+    }
+
+    public String getTxHash() {
+        try {
+            byte[] hash = PKIUtils.hash(_tbs, PKIUtils.ALGORITHM_HASH);
+            return PKIUtils.hexEncode(hash);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String getSignature(String txHash, String hexPrivateKey) {
+        byte[] privateKey = PKIUtils.hexDecode(hexPrivateKey);
+        byte[] hash = PKIUtils.hexDecode(txHash);
+        BigInteger[] sign = PKIUtils.sign(hash, privateKey);
+        byte[] publicKey = PKIUtils.getPublicKeyFromPrivateKey(privateKey, true);
+
+        byte recoveryId = PKIUtils.getRecoveryId(sign, hash, publicKey);
+        byte[] signData = PKIUtils.getSignature(sign[0], sign[1], new byte[]{recoveryId});
+        return PKIUtils.b64Encode(signData);
+    }
 }
