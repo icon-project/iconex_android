01cacf8a37f93516efb9644b08c05add5f98c945
Jeongsang <jeongsang.yoo@icon.foundation>
2018-09-05 10:52:14 +0900

    Add value

--- app/src/main/java/foundation/icon/iconex/MyConstants.java
+++ app/src/main/java/foundation/icon/iconex/MyConstants.java
@@ -25,6 +25,7 @@ public class MyConstants {
     public static final String RESULT_OK = "200";
 
     public static final String PREFIX_ETH = "0x";
+    public static final String PREFIX_IRC = "cx";
 
     // ======== Realm schema version ========
     public static final int VERSION_REALM_SCHEMA = 0;
@@ -52,9 +53,9 @@ public class MyConstants {
     public static final String ETH_INCINERATION = "0x0000000000000000000000000000000000000000";
 
     // ======== ICX Tokens ========
-
-    public static final String CONTRACT_MAIN = "0xb5A5F22694352C15B00323844aD545ABb2B11028";
-    public static final String CONTRACT_TEST = "0x55116b9cf269e3f7e9183d35d65d6c310fcacf05";
+    public static final String IRC_TOKENS_FILE = "IrcTokens.json";
+    public static final String M_ERC_ICX_ADDR = "0xb5A5F22694352C15B00323844aD545ABb2B11028";
+    public static final String T_ERC_ICX_ADDR = "0x55116b9cf269e3f7e9183d35d65d6c310fcacf05";
 
     public static final int ICX_DEC = 18;
     public static final String ICX_SYM = "ICX";
