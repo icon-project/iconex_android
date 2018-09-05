433013c38502f4d9f4204bd77318e174e750d227
Jeongsang <jeongsang.yoo@icon.foundation>
2018-09-05 11:01:48 +0900

    mod struct

--- app/src/main/java/loopchain/icon/wallet/core/request/GetICXCallData.java
+++ app/src/main/java/loopchain/icon/wallet/core/request/GetICXCallData.java
@@ -6,18 +6,14 @@ import loopchain.icon.wallet.core.Constants;
 
 public class GetICXCallData extends RequestData {
 
-    public GetICXCallData(int id, String address, String method) {
+    public GetICXCallData(int id, String address, String score, JsonObject data) {
         this.method = Constants.METHOD_ICXCALL;
         this.id = id;
 
         JsonObject params = new JsonObject();
         params.addProperty("from", address);
-        params.addProperty("to", Constants.ADDRESS_GOVERNANCE);
+        params.addProperty("to", score);
         params.addProperty("dataType", "call");
-
-        JsonObject data = new JsonObject();
-        data.addProperty("method", method);
-
         params.add("data", data);
 
         this.params = params;
