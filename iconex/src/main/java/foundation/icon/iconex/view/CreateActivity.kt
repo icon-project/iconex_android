9d0dc895b8757447b9ab55223b8494872dda9e3d
Jeongsang <jeongsang.yoo@icon.foundation>
2019-08-08 16:30:14 +0900

    layout changed

--- iconex/src/main/java/foundation/icon/iconex/view/CreateActivity.kt
+++ iconex/src/main/java/foundation/icon/iconex/view/CreateActivity.kt
@@ -1,5 +1,7 @@
 package foundation.icon.iconex.view
 
+import android.content.res.Resources
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import foundation.icon.iconex.R
@@ -9,6 +11,8 @@ class CreateActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.decorView.setBackgroundColor(Color.TRANSPARENT)
+
         setContentView(R.layout.create_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
