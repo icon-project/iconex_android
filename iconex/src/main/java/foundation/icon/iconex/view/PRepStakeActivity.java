440b2d6ea451362fedd2de26a987c3b5afebaf03
Jeongsang <jeongsang.yoo@icon.foundation>
2019-09-06 08:55:34 +0900

    Add StakeActivity

---
+++ iconex/src/main/java/foundation/icon/iconex/view/PRepStakeActivity.java
@@ -0,0 +1,16 @@
+package foundation.icon.iconex.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import foundation.icon.iconex.R;
+
+public class PRepStakeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prep_stake);
+    }
+}
